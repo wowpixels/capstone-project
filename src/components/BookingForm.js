13bd804fc26ddd479af50b4a528b1fe466017ea1
Twana Gul <twanagul@twanas-macbook-pro-1.home>
2023-09-29 17:22:54 +0200

    added final pieces of the reservation form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,12 +1,31 @@
 import { useState } from 'react';
 
+const ErrorMessage = () => {
+  return (
+    <p className="absolute right-0 top-7 bg-tertiary rounded text-white px-2 font-bold uppercase text-sm">Required</p>
+  );
+ };
+
 function BookingForm({ availableTimes, dispatchOnDateChange, submitData }) {
     const minDate = new Date().toISOString().split('T')[0];
     const startTime = availableTimes[0];
+    const [firstName, setFirstName] = useState({
+      value: '',
+      isTouched: false,
+    });
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState({
+      value: '',
+      isTouched: false,
+    });
     const [date, setDate] = useState(minDate);
     const [time, setTime] = useState(startTime);
     const [guests, setGuests] = useState(1);
-    const [occasion, setOccasion] = useState('');
+    const [occasion, setOccasion] = useState({
+      value: 'Select',
+      isTouched: false,
+    });
 
     // handle date and time
     const handleDateChange = e => {
@@ -19,19 +38,35 @@ function BookingForm({ availableTimes, dispatchOnDateChange, submitData }) {
     // check if all fields are filled
     const isFormValid = () => {
         return (
+          firstName.isTouched && firstName.value.length > 2 &&
+          phone.isTouched && phone.value.length > 2 &&
           date &&
           time &&
           guests &&
-          occasion
+          occasion.value !== 'Select'
         );
     };
 
+
     // reset form after submit
     const clearForm = () => {
-        setDate("");
-        setTime("");
-        setGuests("");
-        setOccasion("");
+        setFirstName({
+          value: '',
+          isTouched: false,
+        });
+        setLastName('');
+        setEmail('');
+        setPhone({
+          value: '',
+          isTouched: false,
+        });
+        setDate('');
+        setTime('');
+        setGuests('');
+        setOccasion({
+          value: 'Select',
+          isTouched: false,
+        });
     };
 
     const handleSubmit = (e) => {
@@ -42,11 +77,65 @@ function BookingForm({ availableTimes, dispatchOnDateChange, submitData }) {
 
     return (
       <>
-        <form className="grid gap-4 max-w-md" onSubmit={handleSubmit}>
+        <form className="grid grid-cols-1 gap-4 max-w-2xl sm:grid-cols-2" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-1 relative">
+                <label htmlFor="res-date">First Name <span className='text-tertiary'>*</span></label>
+                <input className={firstName.isTouched && firstName.value.length < 2 ?
+                'bg-primary-200 py-2 px-4 border border-primary rounded focus:outline-tertiary border-tertiary' : 'bg-primary-200 py-2 px-4 border border-primary rounded focus:outline-none'}
+                type="text" id="first-name"  placeholder='John' required
+                value={firstName.value}
+                onChange={(e) => {
+                  setFirstName({ ...firstName, value: e.target.value });
+                }}
+                onBlur={() => {
+                  setFirstName({ ...firstName, isTouched: true });
+                }}
+                />
+                {firstName.isTouched && firstName.value.length < 2 ? (
+                  <ErrorMessage />
+                ) : null}
+            </div>
+            <div className="flex flex-col gap-1">
+                <label htmlFor="res-date">Last Name</label>
+                <input className="bg-primary-200 py-2 px-4 border
+                border-primary rounded focus:outline-tertiary" type="text" id="last-name"  placeholder='Doe'
+                value={lastName}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+                />
+            </div>
+            <div className="flex flex-col gap-1">
+                <label htmlFor="res-date">Email</label>
+                <input className="bg-primary-200 py-2 px-4 border
+                border-primary rounded focus:outline-tertiary" type="text" id="full-name"  placeholder='john.doe@email.com'
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                />
+            </div>
+            <div className="flex flex-col gap-1 relative">
+                <label htmlFor="res-date">Phone <span className='text-tertiary'>*</span></label>
+                <input className={phone.isTouched && phone.value.length < 10 ?
+                'bg-primary-200 py-2 px-4 border border-primary rounded focus:outline-tertiary border-tertiary' : 'bg-primary-200 py-2 px-4 border border-primary rounded'}
+                type="text" id="full-name"  placeholder='010 1234567' required
+                value={phone.value}
+                onChange={(e) => {
+                  setPhone({ ...phone, value: e.target.value });
+                }}
+                onBlur={() => {
+                  setPhone({ ...phone, isTouched: true });
+                }}
+                />
+                {phone.isTouched && phone.value.length < 10 ? (
+                  <ErrorMessage />
+                ) : null}
+            </div>
             <div className="flex flex-col gap-1">
                 <label htmlFor="res-date">Choose date <span className='text-tertiary'>*</span></label>
                 <input className="bg-primary-200 py-2 px-4 border
-                border-primary rounded" type="date" id="res-date"
+                border-primary rounded focus:outline-tertiary" type="date" id="res-date" required
                 min={minDate}
                 value={date}
                 onChange={handleDateChange}
@@ -55,7 +144,7 @@ function BookingForm({ availableTimes, dispatchOnDateChange, submitData }) {
             <div className="flex flex-col gap-1">
                 <label htmlFor="res-time">Choose time <span className='text-tertiary'>*</span></label>
                 <select className="bg-primary-200 py-2 px-4 border
-                border-primary rounded" id="res-time"
+                border-primary rounded focus:outline-tertiary" id="res-time"
                 value={time}
                 required={true}
                 onChange={handleTimeChange}
@@ -70,27 +159,34 @@ function BookingForm({ availableTimes, dispatchOnDateChange, submitData }) {
             <div className="flex flex-col gap-1">
                 <label htmlFor="guests">Number of guests <span className='text-tertiary'>*</span></label>
                 <input className="bg-primary-200 py-2 px-4 border
-                border-primary rounded" type="number" placeholder="1"
-                min="1" max="10" id="guests"
+                border-primary rounded focus:outline-tertiary" type="number" placeholder="1"
+                min="1" max="10" id="guests" required
                 value={guests}
                 onChange={(e) => {
                     setGuests(e.target.value);
                   }}
                 />
             </div>
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-1 relative">
                 <label htmlFor="occasion">Occasion <span className='text-tertiary'>*</span></label>
-                <select className="bg-primary-200 py-2 px-4 border
-                border-primary rounded" id="occasion"
-                value={occasion}
+                <select className={occasion.isTouched && occasion.value === 'Select' ?
+                'bg-primary-200 py-2 px-4 border border-primary rounded focus:outline-tertiary border-tertiary' : 'bg-primary-200 py-2 px-4 border border-primary rounded'} id="occasion" required
+                value={occasion.value}
                 onChange={(e) => {
-                    setOccasion(e.target.value);
+                    setOccasion({ ...occasion, value: e.target.value });
                   }}
+                onBlur={() => {
+                  setOccasion({ ...occasion, isTouched: true });
+                }}
                 >
+                    <option>Select</option>
                     <option>Birthday</option>
                     <option>Anniversary</option>
                     <option>Other</option>
                 </select>
+                {occasion.isTouched && occasion.value === 'Select' ? (
+                  <ErrorMessage />
+                ) : null}
             </div>
             <button className="btn mt-4" type="submit" disabled={!isFormValid()}>
                 Make Your Reservation
---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,11 @@
+function ConfirmedBooking() {
+    return (
+      <>
+        <p className="py-4">
+          For assistance with your booking feel free to contact us at 0101234567.
+        </p>
+      </>
+    );
+  }
+
+  export default ConfirmedBooking;
